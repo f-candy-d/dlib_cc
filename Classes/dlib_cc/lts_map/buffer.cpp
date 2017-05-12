e486f324bc1c6fe59b94f16a61c87e43d29db0b5
f-candy-d <pagunouetamuramaro@gmail.com>
2017-05-12 23:18:18 +0900

    extract managing terrain data sysytem as fast-shift-table class

--- Classes/dlib_cc/lts_map/buffer.cpp
+++ Classes/dlib_cc/lts_map/buffer.cpp
@@ -2,86 +2,38 @@
 #include <cassert>
 #include <iostream>
 
-namespace dlib_cc
-{
-namespace lts_map_unit
-{
-
 /**********************************************************************************
  *
  * DLIB_CC::LTS_MAP::BUFFER CLASS
  *
  **********************************************************************************/
 
+namespace dlib_cc
+{
+namespace lts_map_unit
+{
+
 Buffer::Buffer()
-:width_(0)
-,height_(0)
-,que_after_write_(true)
-,row_index_origin_(0, 0)
-,column_index_origin_(0, 0)
+:que_after_write_(true)
 {}
 
 Buffer::Buffer(int w, int h)
-:width_(w)
-,height_(h)
-,que_after_write_(true)
-,row_index_origin_(0, w - 1)
-,column_index_origin_(0, h - 1)
-,terrain_buff_(w * h)
+:que_after_write_(true)
+,terrain_buff_(w, h)
 {
 	assert(0 <= w && 0 <= h);
 }
 
 Buffer::Buffer(int w, int h, int f)
-:width_(w)
-,height_(h)
-,que_after_write_(true)
-,row_index_origin_(0, w - 1)
-,column_index_origin_(0, h - 1)
-,terrain_buff_(w * h, f)
+:que_after_write_(true)
+,terrain_buff_(w, h, f)
 {
 	assert(0 <= w && 0 <= h);
-}
-
-void Buffer::Resize(int w, int h)
-{
-	assert(0 <= w && 0 <= h);
-	terrain_buff_.resize(w * h);
-	width_ = w;
-	height_ = h;
-	row_index_origin_.begin = 0;
-	column_index_origin_.begin = 0;
-	row_index_origin_.end = w - 1;
-	column_index_origin_.end = h - 1;
-}
-
-void Buffer::Resize(int w, int h, int f)
-{
-	assert(0 <= w && 0 <= h);
-	terrain_buff_.resize(w * h, f);
-	width_ = w;
-	height_ = h;
-	row_index_origin_.begin = 0;
-	column_index_origin_.begin = 0;
-	row_index_origin_.end = w - 1;
-	column_index_origin_.end = h - 1;
-}
-
-int Buffer::Size()
-const
-{
-	return width_ * height_;
-}
-
-const int& Buffer::Read(int col, int row)
-const
-{
-	return terrain_buff_[ToIndex(col, row)];
 }
 
 void Buffer::Write(int col, int row, int val)
 {
-	auto index = ToIndex(col, row);
+	auto index = terrain_buff_.to_index(col, row);
 	terrain_buff_[index] = val;
 
 	if(que_after_write_)
@@ -90,198 +42,11 @@ void Buffer::Write(int col, int row, int val)
 	}
 }
 
-void Buffer::ShiftColumns(int distance)
-{
-	column_index_origin_.advance(-1 * distance);
-}
-
-void Buffer::ShiftRows(int distance)
-{
-	row_index_origin_.advance(-1 * distance);
-}
-
-void Buffer::DisableQueuingAfterWrite(bool bol)
-{
-	que_after_write_ = !bol;
-}
-
-void Buffer::Queue(int col, int row)
-{
-	Queue(ToIndex(col, row));
-}
-
 void Buffer::Queue(int index)
 {
-	assert(0 <= index && index < width_ * height_);
+	assert(0 <= index && index < terrain_buff_.size());
 	rendering_que_.push_back(index);
 }
-
-int Buffer::ToIndex(int col, int row)
-const
-{
-	assert(0 <= row && row < height_);
-	assert(0 <= col && col < width_);
-
-	dlib_cc::mobius_strip<int> index(row_index_origin_() * width_, 0, width_ * (height_ - 1), width_);
-	index.advance(row);
-
-	index.tick = 1;
-	index.begin = index();
-	index.end = index.begin + width_ - 1;
-	index.advance(column_index_origin_() + col);
-
-	return index();
-}
-
-/**********************************************************************************
- *
- * ITERATOR OF DLIB_CC::LTS_MAP::BUFFER CLASS
- *
- **********************************************************************************/
-
-using unordered_iterator = Buffer::unordered_iterator;
-using const_unordered_iterator = Buffer::const_unordered_iterator;
-using row_iterator = Buffer::row_iterator;
-using const_row_iterator = Buffer::const_row_iterator;
-using column_iterator = Buffer::column_iterator;
-using const_column_iterator = Buffer::const_column_iterator;
-using difference_type = Buffer::difference_type;
-using size_type = Buffer::size_type;
-using value_type = Buffer::value_type;
-using pointer = Buffer::pointer;
-using const_pointer = Buffer::const_pointer;
-using reference = Buffer::reference;
-using const_reference = Buffer::const_reference;
-
-unordered_iterator Buffer::begin()
-{
-	return terrain_buff_.begin();
-}
-
-unordered_iterator Buffer::end()
-{
-	return terrain_buff_.end();
-}
-
-const_unordered_iterator Buffer::begin() const
-{
-	return terrain_buff_.cbegin();
-}
-
-const_unordered_iterator Buffer::end() const
-{
-	return terrain_buff_.cend();
-}
-
-row_iterator Buffer::begin_row(int row)
-{
-	assert(0 <= row && row < height_);
-
-	auto raw_row = row_index_origin_;
-	raw_row.advance(row);
-	pointer zero = &terrain_buff_[raw_row() * width_];
-	return row_iterator(
-		zero + column_index_origin_(),
-		zero + column_index_origin_(),
-		zero + width_,
-		zero,
-		0,
-		row);
-}
-
-row_iterator Buffer::end_row(int row)
-{
-	auto itr = begin_row(row);
-	itr.base = itr.zero + width_;
-	itr.col = width_;
-	return itr;
-}
-
-const_row_iterator Buffer::begin_row(int row) const
-{
-	assert(0 <= row && row < height_);
-
-	auto raw_row = row_index_origin_;
-	raw_row.advance(row);
-	const_pointer zero = &terrain_buff_[raw_row() * width_];
-	return const_row_iterator(
-		zero + column_index_origin_(),
-		zero + column_index_origin_(),
-		zero + width_,
-		zero,
-		0,
-		row);
-}
-
-const_row_iterator Buffer::end_row(int row) const
-{
-	auto itr = begin_row(row);
-	itr.base = itr.zero + width_;
-	itr.col = width_;
-	return itr;
-}
-
-column_iterator Buffer::begin_column(int col)
-{
-	assert(0 <= col && col < width_);
-
-	auto raw_col = column_index_origin_;
-	raw_col.advance(col);
-	pointer zero = &terrain_buff_[raw_col()];
-	return column_iterator(
-		zero + row_index_origin_() * width_,
-		zero + row_index_origin_() * width_,
-		zero + width_ * height_,
-		zero,
-		col,
-		0,
-		width_);
-}
-
-column_iterator Buffer::end_column(int col)
-{
-	auto itr = begin_column(col);
-	itr.base = itr.zero + width_ * height_;
-	itr.row = height_;
-	return itr;
-}
-
-const_column_iterator Buffer::begin_column(int col) const
-{
-	assert(0 <= col && col < width_);
-
-	auto raw_col = column_index_origin_;
-	raw_col.advance(col);
-	const_pointer zero = &terrain_buff_[raw_col()];
-	return const_column_iterator(
-		zero + row_index_origin_() * width_,
-		zero + row_index_origin_() * width_,
-		zero + width_ * height_,
-		zero,
-		col,
-		0,
-		width_);
-}
-
-const_column_iterator Buffer::end_column(int col) const
-{
-	auto itr = begin_column(col);
-	itr.base = itr.zero + width_ * height_;
-	itr.row = height_;
-	return itr;
-}
-
-/**
- * SPECIALIZATION OF go_forward_coordinate() AND go_backward_coordinate() METHODS IN template_iterator CLASS
- */
-SPECIALIZE_GO_FORWARD_COORDINATE_FUNC_FOR_ROW_ITERATOR(Buffer::row_iterator);
-SPECIALIZE_GO_FORWARD_COORDINATE_FUNC_FOR_ROW_ITERATOR(Buffer::const_row_iterator);
-SPECIALIZE_GO_FORWARD_COORDINATE_FUNC_FOR_COLUMN_ITERATOR(Buffer::column_iterator);
-SPECIALIZE_GO_FORWARD_COORDINATE_FUNC_FOR_COLUMN_ITERATOR(Buffer::const_column_iterator);
-SPECIALIZE_GO_BACKWARD_COORDINATE_FUNC_FOR_ROW_ITERATOR(Buffer::row_iterator);
-SPECIALIZE_GO_BACKWARD_COORDINATE_FUNC_FOR_ROW_ITERATOR(Buffer::const_row_iterator);
-SPECIALIZE_GO_BACKWARD_COORDINATE_FUNC_FOR_COLUMN_ITERATOR(Buffer::column_iterator);
-SPECIALIZE_GO_BACKWARD_COORDINATE_FUNC_FOR_COLUMN_ITERATOR(Buffer::const_column_iterator);
 
 } /* namespace lts_map_unit */
 } /* namespace dlib_cc */
--- Classes/dlib_cc/lts_map/buffer.h
+++ Classes/dlib_cc/lts_map/buffer.h
@@ -2,6 +2,7 @@
 #define DLIB_CC_LTS_MAP_BUFFER_CP_H
 
 #include "../mobius_strip/mobius_strip.h"
+#include "../fast_shift_table/fast_shift_table.h"
 #include <vector>
 
 namespace dlib_cc
@@ -13,271 +14,59 @@ class Buffer
 {
 public:
 	// the value type of elements in this container
-	using value_typename = int;
+	using value_type = int;
+
+	// iterators
+	using unordered_iterator = dlib_cc::fast_shift_table<value_type>::unordered_iterator;
+	using const_unordered_iterator = dlib_cc::fast_shift_table<value_type>::const_unordered_iterator;
+	using row_iterator = dlib_cc::fast_shift_table<value_type>::row_iterator;
+	using const_row_iterator = dlib_cc::fast_shift_table<value_type>::const_row_iterator;
+	using column_iterator = dlib_cc::fast_shift_table<value_type>::column_iterator;
+	using const_column_iterator = dlib_cc::fast_shift_table<value_type>::const_column_iterator;
 
 	Buffer();
 	Buffer(int w, int h);
 	Buffer(int w, int h, int f);
-	void Resize(int w, int h);
-	void Resize(int w, int h, int f);
-	int Size() const;
-	const value_typename& Read(int col, int row) const;
-	void Write(int col, int row, value_typename val);
-	void ShiftColumns(int distance);
-	void ShiftRows(int distance);
-	void DisableQueuingAfterWrite(bool bol);
-	void Queue(int col, int row);
+	void Resize(int w, int h) { terrain_buff_.resize(w, h); }
+	void Resize(int w, int h, int f) { terrain_buff_.resize(w, h, f); }
+	int Size() const { return terrain_buff_.size(); }
+	const value_type& Read(int col, int row) const { return terrain_buff_(col, row); }
+	void Write(int col, int row, value_type val);
+	void ShiftColumns(int distance) { terrain_buff_.shift_columns(distance); }
+	void ShiftRows(int distance) { terrain_buff_.shift_rows(distance); }
+	void DisableQueuingAfterWrite(bool bol) { que_after_write_ = !bol; }
+	void Queue(int col, int row) { Queue(terrain_buff_.to_index(col, row)); }
+
+	unordered_iterator begin() { return terrain_buff_.begin(); }
+	unordered_iterator end() { return terrain_buff_.end(); }
+	const_unordered_iterator begin() const { return terrain_buff_.begin(); }
+	const_unordered_iterator end() const { return terrain_buff_.end(); }
+	row_iterator begin_row(int row) { return terrain_buff_.begin_row(row); }
+	row_iterator end_row(int row) { return terrain_buff_.end_row(row); }
+	const_row_iterator begin_row(int row) const { return std::move(terrain_buff_.begin_row(row)); }
+	const_row_iterator end_row(int row) const { return terrain_buff_.end_row(row); }
+	column_iterator begin_column(int col) { return terrain_buff_.begin_column(col); }
+	column_iterator end_column(int col) { return terrain_buff_.end_column(col); }
+	const_column_iterator begin_column(int col) const { return terrain_buff_.begin_column(col); }
+	const_column_iterator end_column(int col) const { return terrain_buff_.end_column(col); }
 
 	// operators
-	value_typename& operator()(int col, int row) { return terrain_buff_[ToIndex(col, row)]; }
-	const value_typename& operator()(int col, int row) const { return terrain_buff_[ToIndex(col, row)]; }
+	value_type& operator()(int col, int row) { return terrain_buff_(col, row); }
+	const value_type& operator()(int col, int row) const { return terrain_buff_(col, row); }
 
 	// accessors & mutators
-	int width() const { return width_; }
-	int height() const { return height_; }
+	int width() const { return terrain_buff_.width(); }
+	int height() const { return terrain_buff_.height(); }
 	std::vector<int>& rendering_queue () { return rendering_que_; }
 	const std::vector<int>& rendering_queue () const { return rendering_que_; }
 
 private:
-	int width_;
-	int height_;
 	bool que_after_write_;
-	dlib_cc::mobius_strip<int> row_index_origin_;
-	dlib_cc::mobius_strip<int> column_index_origin_;
-	std::vector<value_typename> terrain_buff_;
+	dlib_cc::fast_shift_table<value_type> terrain_buff_;
 	std::vector<int> rendering_que_;
 
 	void Queue(int index);
-	int ToIndex(int col, int row) const;
-
-	/**********************************************************************************
-	 *
-	 * ITERATOR OF BUFFER CLASS
-	 *
-	 **********************************************************************************/
-private:
-	/**
-	 * the following structures are invisible from the other classes,
-	 * and that have no body (only used for specialization of template_iterator class)
-	 */
-	struct row_iterator_type;
-	struct column_iterator_type;
-	struct const_row_iterator_type;
-	struct const_column_iterator_type;
-
-public:
-	using difference_type = std::ptrdiff_t;
-	using size_type = std::size_t;
-	using value_type = value_typename;
-	using pointer = value_type*;
-	using const_pointer = const value_type*;
-	using reference = value_type&;
-	using const_reference = const value_type&;
-
-	/**
-	 * iterator names
-	 */
-	template <typename iterator_type, typename reference_type, typename pointer_type> struct template_iterator;
-	using row_iterator = template_iterator<row_iterator_type, reference, pointer>;
-	using column_iterator = template_iterator<column_iterator_type, reference, pointer>;
-	using const_row_iterator = template_iterator<const_row_iterator_type, const_reference, const_pointer>;
-	using const_column_iterator = template_iterator<const_column_iterator_type, const_reference, const_pointer>;
-	using unordered_iterator = std::vector<value_typename>::iterator;
-	using const_unordered_iterator = std::vector<value_typename>::const_iterator;
-
-	/**
-	* GETTER METHODS FOR ITERATORS
-	*/
-	unordered_iterator begin();
-	unordered_iterator end();
-	const_unordered_iterator begin() const;
-	const_unordered_iterator end() const;
-	row_iterator begin_row(int row);
-	row_iterator end_row(int row);
-	const_row_iterator begin_row(int row) const;
-	const_row_iterator end_row(int row) const;
-	column_iterator begin_column(int col);
-	column_iterator end_column(int col);
-	const_column_iterator begin_column(int col) const;
-	const_column_iterator end_column(int col) const;
-
-	/**
-	 * TEMPLATE CLASS OF ITERATORS
-	 */
-	template <typename iterator_type, typename reference_type, typename pointer_type> struct template_iterator : public std::iterator<
-		std::random_access_iterator_tag,
-		value_type,
-		difference_type,
-		pointer_type,
-		reference_type>
-	{
-	public:
-		// a pointer to an elements in a container
-		pointer_type base;
-		pointer_type begin;
-		pointer_type end;
-		pointer_type zero;
-		int col;
-		int row;
-		difference_type tick;
-
-		template_iterator()
-		:base(),begin(),end(),zero(),col(),row(),tick()
-		{}
-
-		template_iterator(
-			pointer_type bs, pointer_type bg, pointer_type ed, pointer_type zr, int c, int r, difference_type tc = 1)
-		:base(bs),begin(bg),end(ed),zero(zr),col(c),row(r),tick(tc)
-		{
-			assert(zero <= begin && begin <= end);
-		}
-
-		template_iterator(const template_iterator& other)
-		:base(other.base),begin(other.begin),end(other.end),zero(other.zero),col(other.col),row(other.row),tick(other.tick)
-		{}
-
-		difference_type operator-(const template_iterator& other)
-		const
-		{
-			return static_cast<difference_type>(base - other.base);
-		}
-
-		template_iterator& operator=(const template_iterator& other)
-		{
-			if(this != &other)
-			{
-				base = other.base;
-				begin = other.begin;
-				end = other.end;
-				zero = other.zero;
-			}
-
-			return *this;
-		}
-
-		template_iterator& operator++()
-		{
-			go_forward_coordinate();
-
-			if(zero == begin)
-			{
-				base += tick;
-				return *this;
-			}
-			else if(base + tick == zero)
-				base = begin;
-			else if(base + tick == end)
-				base = zero;
-			else if(base + tick == begin)
-				base = end;
-			else
-				base += tick;
-
-			return *this;
-		}
-
-		template_iterator operator++(int)
-		{
-			auto old = *this;
-			++*this;
-			return old;
-		}
-
-		template_iterator& operator--()
-		{
-			go_backward_coordinate();
-
-			if(zero == begin)
-			{
-				base -= tick;
-				return *this;
-			}
-			else if(base == end)
-				base = begin - tick;
-			else if(base == zero)
-				base = end - tick;
-			else if(base == begin)
-				base = zero - tick;
-			else
-				base -= tick;
-
-			return *this;
-		}
-
-		template_iterator operator--(int)
-		{
-			auto old = *this;
-			--*this;
-			return old;
-		}
-
-		template_iterator& operator+=(difference_type n)
-		{
-			if(n < 0)
-			{
-				for(int i = 0; i < -1 * n; ++i)
-					--*this;
-			}
-			else
-			{
-				for(int i = 0; i < n; ++i)
-					++*this;
-			}
-
-			return *this;
-		}
-
-		template_iterator operator+(difference_type n)
-		const
-		{
-			auto cp = *this;
-			return (cp += n);
-		}
-
-		template_iterator& operator-=(difference_type n)
-		{
-			return (*this += -n);
-		}
-
-		template_iterator operator-(difference_type n)
-		const
-		{
-			auto cp = *this;
-			return (cp -= n);
-		}
-
-		reference_type operator*() const { return *base; }
-		pointer_type operator->() const { return &**this; }
-		bool operator==(const template_iterator& other) const { return (base == other.base); }
-		bool operator!=(const template_iterator& other) const { return !(*this == other); }
-		bool operator<(const template_iterator& other) const { return (base < other.base); }
-		bool operator>(const template_iterator& other) const { return  (other < *this); }
-		bool operator<=(const template_iterator& other) const { return !(*this > other); }
-		bool operator>=(const template_iterator& other) const { return  !(*this < other); }
-
-	private:
-		/**
-		 * do specialization these methods in .cpp file for (const-)row-iterator and (const-)column-iterator
-		 */
-		void go_forward_coordinate();
-		void go_backward_coordinate();
-	};
 };
-
-/**
- * macros for specialization of go_forward_coordinate() and go_backward_coordinate() methods
- */
-#define SPECIALIZE_GO_FORWARD_COORDINATE_FUNC_FOR_ROW_ITERATOR(iterator_type)\
-template <> void Buffer::iterator_type::go_forward_coordinate() { ++col; }\
-
-#define SPECIALIZE_GO_BACKWARD_COORDINATE_FUNC_FOR_ROW_ITERATOR(iterator_type)\
-template <> void Buffer::iterator_type::go_backward_coordinate() { --col; }\
-
-#define SPECIALIZE_GO_FORWARD_COORDINATE_FUNC_FOR_COLUMN_ITERATOR(iterator_type)\
-template <> void Buffer::iterator_type::go_forward_coordinate() { ++row; }\
-
-#define SPECIALIZE_GO_BACKWARD_COORDINATE_FUNC_FOR_COLUMN_ITERATOR(iterator_type)\
-template <> void Buffer::iterator_type::go_backward_coordinate() { --row; }\
 
 } /* namespace lts_map_unit */
 } /* namespace dlib_cc */
